4c63068c6195002e1f7bba2bc985463e0d4bd115
AleexAlvz <61116687+AleexAlvz@users.noreply.github.com>
2021-05-04 19:57:59 -0300

    Conserto de importacao da classe User, evitando os crash do app

--- app/src/main/java/br/com/brq/listdelivery/model/Tarefas.kt
+++ app/src/main/java/br/com/brq/listdelivery/model/Tarefas.kt
@@ -1,12 +1,14 @@
 package br.com.brq.meuprimeiroapp.model
 
+import br.com.brq.listdelivery.model.User
+
 class Tarefas (
-    var id : String,
-    var usuario : User,
-    var status : StatusPedido,
-    var dataEntrega : String,
-    var pedido : Pedido,
-    var observacao : String
+        var id : String,
+        var usuario : User,
+        var status : StatusPedido,
+        var dataEntrega : String,
+        var pedido : Pedido,
+        var observacao : String
 ){
     //Metodos para tarefas
 }
