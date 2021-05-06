00fc6c5cbb711c540ef7a65d1d341d938c1ba8cb
Agatha886 <agathamonfredini14@gmail.com>
2021-05-05 21:53:05 -0300

    teste com listas

---
+++ app/src/main/java/br/com/brq/listdelivery/model/Tarefas.kt
@@ -0,0 +1,12 @@
+package br.com.brq.listDelivery.model
+
+class Tarefas (
+    var id : String,
+//    var usuario : User,
+    var status : StatusPedido,
+    var dataEntrega : String,
+    var pedido : Pedido,
+    var observacao : String
+){
+    //Metodos para tarefas
+}
